ccf4fbdea1f7ac33cf81e373f4bf0f53b1b9b9a7
gqlcms <qilong.guo@cern.ch>
2022-02-04 19:42:49 +0800

    update JME module

--- modules/VVVProducer.py
+++ modules/VVVProducer.py
@@ -10,18 +10,33 @@ import os
 import numpy as np
 from numpy import sign
 
-def Process_1Lepton_2fatJets(event):
+def Process_0Lepton(event):
+    if ( event.nFatJet < 2 ): return False
+    return True
+
+def Process_1Lepton(event):
     if ( event.nFatJet < 2 ): return False
     if ( (event.nElectron+event.nMuon) < 1 ): return False
     return True
+
+def Process_2Lepton(event):
+    if ( (event.nElectron+event.nMuon) < 2 ): return False
+    return True
+
+def Process_4Lepton(event):
+    if ( (event.nElectron+event.nMuon) < 4 ): return False
+    return True
+
 
 class VVVProducer(Module):
   def __init__(self , year, MODE = "1Lepton" ):
     self.year = year
     self.MODE = MODE
     self.Process_Genparticles = False
-    if self.MODE == "1Lepton":
-        self.function = Process_1Lepton_2fatJets
+    if self.MODE == "0Lepton": self.function = Process_0Lepton
+    if self.MODE == "1Lepton": self.function = Process_1Lepton
+    if self.MODE == "2Lepton": self.function = Process_2Lepton
+    if self.MODE == "4Lepton": self.function = Process_4Lepton
 
   def beginJob(self):
     pass
@@ -30,19 +45,27 @@ class VVVProducer(Module):
     pass
 
   def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
-    self.out = wrappedOutputTree
+    self.out   = wrappedOutputTree
     self.is_mc = bool(inputTree.GetBranch("GenJet_pt"))
 
   def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
     pass
 
   def analyze(self, event):
-
     # PV selection
-
     return (self.function)(event)
 
+VVV2016_0Lepton = lambda MODE="inclusive": VVVProducer("2016","0Lepton")
+VVV2016_1Lepton = lambda MODE="inclusive": VVVProducer("2016","1Lepton")
+VVV2016_2Lepton = lambda MODE="inclusive": VVVProducer("2016","2Lepton")
+VVV2016_4Lepton = lambda MODE="inclusive": VVVProducer("2016","4Lepton")
 
-VVV2016 = lambda MODE="inclusive": VVVProducer("2016",MODE)
-VVV2017 = lambda MODE="inclusive": VVVProducer("2017",MODE)
-VVV2018 = lambda MODE="inclusive": VVVProducer("2018",MODE)
+VVV2017_0Lepton = lambda MODE="inclusive": VVVProducer("2017","0Lepton")
+VVV2017_1Lepton = lambda MODE="inclusive": VVVProducer("2017","1Lepton")
+VVV2017_2Lepton = lambda MODE="inclusive": VVVProducer("2017","2Lepton")
+VVV2017_4Lepton = lambda MODE="inclusive": VVVProducer("2017","4Lepton")
+
+VVV2018_0Lepton = lambda MODE="inclusive": VVVProducer("2018","0Lepton")
+VVV2018_1Lepton = lambda MODE="inclusive": VVVProducer("2018","1Lepton")
+VVV2018_2Lepton = lambda MODE="inclusive": VVVProducer("2018","2Lepton")
+VVV2018_4Lepton = lambda MODE="inclusive": VVVProducer("2018","4Lepton")
--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,17 @@
-python run.py -m -i /stash/user/qilongguo/work/aQGC_Production/Output/UL/2018/ST_Scale/WWW/File_10608484_9_50_WWW-RunIISummer20UL18NanoAODv9_50.root -o ./ --year 2016pre -M 1Lepton
-# python run.py -m -i /stash/user/qilongguo/work/aQGC_Production/Output/UL/2018/ST_Scale/WWW/File_10608484_9_50_WWW-RunIISummer20UL18NanoAODv9_50.root -o ./ --year 2018
-# python run.py -m -i /stash/user/qilongguo/work/aQGC_Production/Output/UL/2018/ST_Scale/WWW/File_10608484_9_50_WWW-RunIISummer20UL18NanoAODv9_50.root -o ./ --year 2016post -M 1Lepton
+python run.py -m -i /stash/user/qilongguo/public/VVV_EFT/private_sample/aQGC/WWW/WWW_0.root --year 2016pre 
 
+/home/gql/tutorial/lib/WVV-aQGC-study/FrameWork/Nanotool/Code/v3
 
+rm -r data
+rm -r modules
+rm -r others
+rm -r scripts
+rm -r test/crab_auto/
+
+data
+modules
+others
+scripts
+test
+
+cp -r /home/gql/tutorial/lib/WVV-aQGC-study/FrameWork/Nanotool/Code/v3/$1 .
